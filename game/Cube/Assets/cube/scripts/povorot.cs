8d5f9d043ed9f601cbaddf41a4206bbe80d3b4c7
Lagumir <Legals92@mail.ru>
2020-11-07 13:37:11 +0300

    fjif

--- game/Cube/Assets/cube/scripts/povorot.cs
+++ game/Cube/Assets/cube/scripts/povorot.cs
@@ -13,9 +13,12 @@ public class povorot : MonoBehaviour
         float hitdist = 0.0f;
         if (playerPlane.Raycast(ray, out hitdist))
         {
-            Vector3 targetPoint = ray.GetPoint(hitdist);
-            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Vector3 targetPoint = ray.GetPoint(hitdist);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+            }
         }
     }
 }
